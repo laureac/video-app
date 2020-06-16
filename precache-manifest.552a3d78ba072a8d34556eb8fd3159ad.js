fc4b195f747fcf90ea9b7351173356dceb988528
Laure Claret <laure.a.claret@gmail.com>
2020-06-16 22:08:41 +1000

    Updates

---
+++ precache-manifest.552a3d78ba072a8d34556eb8fd3159ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79cfb6be03f3181950c9df5dffda60c5",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "efaabc1530255b192683",
+    "url": "/video-app/static/css/main.f9ccaf6f.chunk.css"
+  },
+  {
+    "revision": "2b11694d0e9910609386",
+    "url": "/video-app/static/js/2.62c6c4c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/video-app/static/js/2.62c6c4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efaabc1530255b192683",
+    "url": "/video-app/static/js/main.411c1e90.chunk.js"
+  },
+  {
+    "revision": "3981d1297c696bdd298e",
+    "url": "/video-app/static/js/runtime-main.3a4e6470.js"
+  }
+]);
